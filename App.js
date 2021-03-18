f28a00b40546d09586c66e2b3d82376f6331c1ae
candytuft <vishnyc3@gmail.com>
2021-03-17 22:33:07 -0400

    Adds button for learning purposes

--- App.js
+++ App.js
@@ -1,28 +1,16 @@
 import React from "react";
-import {
-  StyleSheet,
-  Text,
-  TouchableHighlight,
-  Image,
-  SafeAreaView,
-} from "react-native";
+import { StyleSheet, SafeAreaView, Button } from "react-native";
 
 export default function App() {
   const handlePress = () => console.log("Text Pressed!");
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Round Off</Text>
-      <TouchableHighlight onPress={() => console.log("Image tapped")}>
-        <Image
-          blurRadius={5}
-          source={{
-            width: 200,
-            height: 300,
-            uri: "https://picsum.photos/200/300",
-          }}
-        />
-      </TouchableHighlight>
+      <Button
+        color="black"
+        title="Click me"
+        onPress={() => console.log("button tapped")}
+      />
     </SafeAreaView>
   );
 }
