4205e8522f1ef57ac818a1bed174251643acb761
candytuft <vishnyc3@gmail.com>
2021-03-17 22:29:50 -0400

    Adds Touchables to the App for learning purposes

--- App.js
+++ App.js
@@ -1,16 +1,28 @@
-import React from 'react';
-import { StyleSheet, Text, View, Image, SafeAreaView} from 'react-native';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  Image,
+  SafeAreaView,
+} from "react-native";
 
 export default function App() {
-  const handlePress = () => console.log('Text Pressed!');
+  const handlePress = () => console.log("Text Pressed!");
 
   return (
     <SafeAreaView style={styles.container}>
       <Text>Round Off</Text>
-      <Image source={{
-        width: 200,
-        height: 300,
-        uri: 'https://picsum.photos/200/300' }} />
+      <TouchableHighlight onPress={() => console.log("Image tapped")}>
+        <Image
+          blurRadius={5}
+          source={{
+            width: 200,
+            height: 300,
+            uri: "https://picsum.photos/200/300",
+          }}
+        />
+      </TouchableHighlight>
     </SafeAreaView>
   );
 }
@@ -18,8 +30,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
