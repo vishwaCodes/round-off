efecceccad162e7843773cd45b67d9b30b063bf4
candytuft <vishnyc3@gmail.com>
2021-03-19 22:15:59 -0400

    Adds buttons to View Image Screen

--- app/screens/ViewImageScreen.js
+++ app/screens/ViewImageScreen.js
@@ -5,6 +5,8 @@ import { Image, StyleSheet } from "react-native";
 function ViewImageScreen(props) {
   return (
     <View style={styles.container}>
+      <View style={styles.closeIcon}></View>
+      <View style={styles.deleteIcon}></View>
       <Image
         resizeMode="contain"
         style={styles.image}
@@ -15,9 +17,25 @@ function ViewImageScreen(props) {
 }
 
 const styles = StyleSheet.create({
+  closeIcon: {
+    width: 50,
+    height: 50,
+    backgroundColor: "#ACE1AF",
+    position: "absolute",
+    top: 70,
+    left: 30,
+  },
   container: {
     backgroundColor: "#15202b",
     flex: 1,
+  },
+  deleteIcon: {
+    width: 50,
+    height: 50,
+    backgroundColor: "#A9BA9D",
+    position: "absolute",
+    top: 70,
+    right: 30,
   },
   image: {
     width: "100%",
